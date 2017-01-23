438da3c296b785210aaf1151689dd7c9fc193a97
Andy Khmara <andykhmara@gmail.com>
2017-01-23 23:12:50 +0200

    changed name of main component

--- src/front-end/js/app.jsx
+++ src/front-end/js/app.jsx
@@ -4,7 +4,7 @@ import Header from './components/header.jsx';
 import Content from './components/content.jsx';
 import Footer from './components/footer.jsx';
 
-class World extends React.Component {
+class Harley extends React.Component {
   render() {
     return (
     	<div className="row">
@@ -16,4 +16,4 @@ class World extends React.Component {
   }
 }
  
-ReactDOM.render(<World/>, document.getElementById('app'));
+ReactDOM.render(<Harley/>, document.getElementById('app'));
