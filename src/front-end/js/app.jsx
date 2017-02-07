ef3cc9dc637c3f7838a9fc60295be95cb744eb4b
DrobenyukA <drobenyuk.a@gmail.com>
2017-02-07 21:08:05 +0200

    update app.js

--- src/front-end/js/app.jsx
+++ src/front-end/js/app.jsx
@@ -6,44 +6,44 @@ import Header        from './components/header.jsx';
 import Content       from './components/content.jsx';
 import Footer        from './components/footer.jsx';
 
-import { CHART_TYPES } from './constants/constants.jsx';
+import {CHART_TYPES} from './constants/constants.jsx';
 
-import { getWeatherData } from './actions/dataActions.jsx';
+import {getWeatherData} from './actions/dataActions.jsx';
 import store from './stores/harleyStore.jsx';
 
 console.log(store);
 
 class Harley extends React.Component {
-  constructor () {
-    super();
+    constructor() {
+        super();
 
-    this.unsubscribe = store.subscribe(() => {
-        this.setState(store.getState());
-      }
-    );
+        this.unsubscribe = store.subscribe(() => {
+                this.setState(store.getState());
+            }
+        );
 
-    this.state = store.getState();
-    getWeatherData();
-  }
+        this.state = store.getState();
+        getWeatherData();
+    }
 
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
 
-  render() {
-    console.log('app.jsx', this.state);
-    return (
-    	<div className="row">
-		      <Header
-	        />
-	        <Content
-	          chartType={this.state.chart.chartType}
-            weather={this.state.weather.weather}
-	        />	        
-	        <Footer/>
-    	</div>
-    );
-  }
+    render() {
+        console.log('app.jsx', this.state);
+        return (
+            <div className="row">
+                <Header
+                />
+                <Content
+                    chartType={this.state.chart.chartType}
+                    weather={this.state.weather.weather}
+                />
+                <Footer/>
+            </div>
+        );
+    }
 }
- 
+
 ReactDOM.render(<Harley/>, document.getElementById('app'));
