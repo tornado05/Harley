6dabe9616b85eb117ede724731c132fc38b80ab9
Andy Khmara <andykhmara@gmail.com>
2017-02-14 23:39:58 +0200

    add render statistic chart by services + fix problem on backend with calculation statistics

--- src/front-end/js/stores/harleyStore.jsx
+++ src/front-end/js/stores/harleyStore.jsx
@@ -2,14 +2,16 @@ import harleyReducer from "./../reducers/harleyReducer.jsx";
 import {createStore} from "redux";
 import {CHART_TYPES} from "./../constants/constants.jsx";
 
-let initialDate = new Date().toISOString();
+let date = new Date();
+let firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1).toISOString();
+let lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).toISOString();
 
 let store = createStore(harleyReducer, {
     chart: {
         chartType: CHART_TYPES.TEMPERATURE,
         cityName: "Rivne",
-        periodFrom: initialDate,
-        periodTo: initialDate,
+        periodFrom: firstDayOfMonth,
+        periodTo: lastDayOfMonth,
         statType: CHART_TYPES.TEMPERATURE
     },
     weather: {
