658f4a9c198aa3d8fa84f459575237db1897f285
GagarinAP <gagarin.web@gmail.com>
2017-02-18 17:54:09 +0200

    g_react

--- src/front-end/js/app.jsx
+++ src/front-end/js/app.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import Header from "./components/header.jsx";
 import Content from "./components/content.jsx";
 import Footer from "./components/footer.jsx";
