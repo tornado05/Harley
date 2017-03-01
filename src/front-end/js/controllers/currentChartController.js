d2b3b427aebe00713cfa7b7a0374c7650235ab5d
DrobenyukA <drobenyuk.a@gmail.com>
2017-03-01 22:07:12 +0200

    refactor map and current chart controllers

--- src/front-end/js/controllers/currentChartController.js
+++ src/front-end/js/controllers/currentChartController.js
@@ -1,62 +1,63 @@
 Harley.controller("currentChartController", [
     "$rootScope", "$scope", '$http', 'WeatherService',
     function ($rootScope, $scope, $http, WeatherService) {
-        var initialize = function (){
+        var initialize = function () {
+            angular.extend($scope, {
+                labels: [],
+                data: [],
+                options: {},
+                cities: [{
+                    label: "city",
+                    value: "city"
+                }],
+                params: [{
+                    label: 'value',
+                    value: 'value'
+                }]
+            });
             getConfigs();
             setSelectedOptions();
         };
-        $rootScope.$watch('currentWeather', function (){
+
+        $rootScope.$watch('currentWeather', function () {
             $scope.updateChart();
         });
 
-        var getConfigs = function (){
+        var getConfigs = function () {
             $http({
                 method: 'GET',
                 url: '/weather/v01/configs'
-            }).then(function(res){
+            }).then(function (res) {
                 $scope.cities = res.data.cities;
                 $scope.params = res.data.params;
                 setSelectedOptions();
-            },function(res){
+            }, function (res) {
                 console.log('Loading configs failed! Code: ', res.statusCode)
             });
         };
 
-        $scope.cities = [
-            {
-                label: "city",
-                value: "city"
-            }
-        ];
-        $scope.params = [
-            {
-                label: 'value',
-                value: 'value'
-            }
-        ];
-
-        var setSelectedOptions = function (){
+        var setSelectedOptions = function () {
             $scope.selectedCity = _.first($scope.cities).value;
             $scope.selectedParam = _.first($scope.params).name;
         };
 
-        $scope.updateChart = function(){
+        $scope.updateChart = function () {
             $scope.labels = [];
             $scope.data = [];
             $scope.options = updateChartOptions();
-            _.each($rootScope.currentWeather, function (data){
-                if ((data.cityName == $scope.selectedCity)){
+            _.each($rootScope.currentWeather, function (data) {
+                if ((data.cityName == $scope.selectedCity)) {
                     $scope.labels.push(data.sourceAPI);
                     $scope.data.push(data[$scope.selectedParam])
                 }
             });
         };
 
-        var getTicksByParam = function (){
+        var getTicksByParam = function () {
             var result = {};
-                result.beginAtZero = true;
-            _.each($scope.params, function (param){
-                if (param.name === $scope.selectedParam){
+            result.beginAtZero = true;
+            _.each($scope.params, function (param) {
+                if (param.name === $scope.selectedParam) {
                     result.max = param.max;
                     result.min = param.min;
                     $scope.series = param.label;
@@ -65,8 +66,8 @@ Harley.controller("currentChartController", [
             return result;
         };
 
-        var updateChartOptions = function(){
-           return {
+        var updateChartOptions = function () {
+            return {
                 responsive: true,
                 scales: {
                     yAxes: [{
--- src/front-end/js/controllers/mapController.js
+++ src/front-end/js/controllers/mapController.js
@@ -1,88 +1,102 @@
 Harley.controller("mapController", [
-    "$rootScope", "$scope", '$http', 'WeatherService',
-    function ($rootScope, $scope, $http, WeatherService) {
+    "$rootScope", "$scope", '$http', 'WeatherService', 'MAPBOX', 'OPENSTREET', 'MAP',
+    function ($rootScope, $scope, $http, WeatherService, MAPBOX, OPENSTREET, MAP) {
 
-    var initialize = function () {
-        //TODO: take this values from user config, by default from constants
-        angular.extend($scope, {
-            center: {
-                lat: 50.9,
-                lng: 27.8,
-                zoom: 7
-            },
-            defaults: {
+        var initialize = function () {
+            angular.extend($scope, {
+                center: {
+                    lat: MAP.POSITION.LATITUDE,
+                    lng: MAP.POSITION.LONGITUDE,
+                    zoom: MAP.BASE_ZOOM
+                },
+                defaults: getMapOptions(),
+                layers: {
+                    baselayers: {
+                        mapbox: getMapboxConfig(),
+                        osm: getOpenStreetConfig()
+                    }
+                }
+            });
+            getCurrentWeatherData();
+        };
+
+        var getMapOptions = function (){
+            return {
                 zoomControlPosition: 'topleft',
                 tileLayerOptions: {
                     detectRetina: true,
                     reuseTiles: true
                 },
                 scrollWheelZoom: false
-            },
-            layers: {
-                baselayers: {
-                    mapbox_light: {
-                        name: 'mapbox.streets',
-                        url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
-                        type: 'xyz',
-                        layerOptions: {
-                            apikey: 'pk.eyJ1IjoiZHJvYmVueXVrIiwiYSI6ImNpdXp3aDczZTAwM2wyb3IzbXF0OTZ5YjgifQ.2WbUs9CJ8XuPlG3coCxBbg',
-                            mapid: 'mapbox.streets'
-                        }
-                    },
-                    osm: {
-                        name: 'OpenStreetMap',
-                        url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                        type: 'xyz'
-                    }
+            };
+        };
+
+        var getCurrentWeatherData = function(){
+            $http.get('/weather/v01/current')
+                .then(function (res) {
+                    var service = $rootScope.preferedService || "darkSky";
+                    var data = WeatherService.getDataByService(res.data, service);
+                    //TODO: move to module which will be responsible for data grab
+                    $rootScope.currentWeather = res.data;
+                    renderMarkers(data);
+                }, function (res) {
+                    console.log("Something went wrong:", res.statusText);
+                });
+        };
+
+        var getMapboxConfig = function(){
+            return {
+                name: MAPBOX.NAME,
+                url: MAPBOX.URL,
+                type: MAPBOX.TYPE,
+                layerOptions: {
+                    apikey: MAPBOX.API_KEY,
+                    mapid: MAPBOX.TILES_NAME
                 }
             }
-        });
-        $http.get('/weather/v01/current')
-            .then(function (response) {
-                var service = $rootScope.preferedService || "darkSky";
-                var data = WeatherService.getDataByService(response.data, service);
-                //TODO: move to module which will be responsible for data grab
-                $rootScope.currentWeather = response.data;
-                renderMarkers(data);
-            }, function (response) {
-                //Handles error
-                console.log("Something went wrong:", response.statusText);
-            });
-    };
+        };
 
-    var prepareMarkerMsg = function (data) {
-        return [
-            '<h4>',
-            data.cityName,
-            '</h4><ul><li>Temperature: <b>',
-            data. temp,
-            ' &deg;C</b></li>',
-            '<li>Pressure: <b>',
-            data.pressure,
-            ' mmHg</b></li>',
-            '<li>Humidity: <b>',
-            data.humidity,' %</b></li>',
-            '<li>Wind speed: <b>',
-            data.windSpeed,
-            ' meter/sec</b></li></ul>'
-        ].join('')
-    };
+        var getOpenStreetConfig = function(){
+            return {
+                name: OPENSTREET.NAME,
+                url: OPENSTREET.URL,
+                type: OPENSTREET.TYPE
+            };
+        };
 
-    var renderMarkers = function (data) {
-        var markers = [];
-        _.each(data, function (item){
-            markers.push({
-                lat: item.coords.lat,
-                lng: item.coords.lon,
-                focus: false,
-                draggable: false,
-                message: prepareMarkerMsg(item),
-                icon: {}
+        var prepareMarkerMsg = function (data) {
+            return [
+                '<h4>',
+                data.cityName,
+                '</h4><ul><li>Temperature: <b>',
+                data.temp,
+                ' &deg;C</b></li>',
+                '<li>Pressure: <b>',
+                data.pressure,
+                ' mmHg</b></li>',
+                '<li>Humidity: <b>',
+                data.humidity, ' %</b></li>',
+                '<li>Wind speed: <b>',
+                data.windSpeed,
+                ' meter/sec</b></li></ul>'
+            ].join('')
+        };
+
+        var renderMarkers = function (data) {
+            var markers = [];
+            _.each(data, function (item) {
+                markers.push({
+                    lat: item.coords.lat,
+                    lng: item.coords.lon,
+                    focus: false,
+                    draggable: false,
+                    message: prepareMarkerMsg(item),
+                    icon: {}
+                });
             });
-        });
-        angular.extend($scope, {
-            markers: markers
-        })
-    };
-    initialize();
-}]);
+            angular.extend($scope, {
+                markers: markers
+            })
+        };
+        initialize();
+    }]);
